21e208e98954df301a849c61c08e5f368aeddbe1
Jagannath Bhat <jagannathbhatjb@gmail.com>
2020-03-29 12:08:39 +0530

    add frontend

---
+++ client/src/App.js
@@ -0,0 +1,131 @@
+import 'typeface-roboto'
+import axios from 'axios'
+import React, { useState } from 'react'
+import {
+	Button,
+	CircularProgress,
+	createMuiTheme,
+	InputAdornment,
+	Paper,
+	TextField,
+	ThemeProvider,
+	Typography,
+	useMediaQuery
+} from '@material-ui/core'
+
+import './App.css'
+import { useStyles } from './theme'
+
+const App = () => {
+	const [info, setInfo] = useState(null)
+	const [loading, setLoading] = useState(false)
+
+	const classes = useStyles()
+
+	const submitHandler = async event => {
+		event.preventDefault()
+		setLoading(true)
+
+		const animeName = document.getElementById('animeNameInput').value
+
+		if (animeName === '') {
+			setInfo({ name: 'No name entered', links: [] })
+			setLoading(false)
+			return
+		}
+
+		try {
+			const res = await axios.post(
+				'/generate',
+				{ name: animeName },
+				{ headers: { 'Content-Type': 'application/json' } }
+			)
+			setInfo(res.data)
+			setLoading(false)
+		} catch (err) {
+			console.log(err)
+			setInfo({ name: 'Generation Failed', links: [] })
+			setLoading(false)
+		}
+	}
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Paper
+				elevation={0}
+				className={
+					useMediaQuery('(min-width:660px)')
+						? classes.root + ' ' + classes.rootBig
+						: classes.root
+				}
+			>
+				<header>
+					<Typography
+						variant={useMediaQuery('(min-width:660px)') ? 'h1' : 'h3'}
+						className={classes.title}
+					>
+						AnimeYoutube
+						<br />
+						Downloader
+					</Typography>
+					<form className={classes.form} onSubmit={submitHandler}>
+						<TextField
+							autoFocus
+							id='animeNameInput'
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position='start'>
+										animeyoutube.com/
+									</InputAdornment>
+								)
+							}}
+							placeholder='name-of-anime'
+						/>
+						<div className={classes.wrapper}>
+							<Button disabled={loading} type='submit'>
+								Generate Links
+							</Button>
+							{loading && (
+								<CircularProgress
+									size={24}
+									className={classes.buttonProgress}
+								/>
+							)}
+						</div>
+					</form>
+				</header>
+				<main className={classes.main}>
+					{info !== null ? (
+						<>
+							{info.img ? (
+								<img src={info.img} alt={info.name + ' poster'} />
+							) : (
+								<></>
+							)}
+							<Typography component='h2' variant='h4'>
+								{info.name}
+							</Typography>
+							<div className={classes.linkList}>
+								{info.links.map((link, key) => (
+									<a download href={link} key={key} rel='noopener noreferrer'>
+										<Button>Episode {key + 1}</Button>
+									</a>
+								))}
+							</div>
+						</>
+					) : (
+						<></>
+					)}
+				</main>
+			</Paper>
+		</ThemeProvider>
+	)
+}
+
+const theme = createMuiTheme({
+	palette: {
+		type: 'dark'
+	}
+})
+
+export default App
---
+++ client/src/theme.js
@@ -0,0 +1,45 @@
+import { makeStyles } from '@material-ui/core'
+import { green } from '@material-ui/core/colors'
+
+export const useStyles = makeStyles(theme => ({
+	buttonProgress: {
+		color: green[500],
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		marginTop: -12,
+		marginLeft: -12
+	},
+	form: {
+		alignItems: 'start',
+		display: 'flex',
+		flexDirection: 'column',
+		marginTop: theme.spacing(4),
+		'& > *': {
+			marginTop: theme.spacing(2)
+		}
+	},
+	linkList: {
+		display: 'flex',
+		flexDirection: 'column',
+		marginTop: theme.spacing(3)
+	},
+	main: {
+		margin: theme.spacing(3) + 'px 0px'
+	},
+	root: {
+		borderRadius: '0px',
+		flex: 1,
+		padding: theme.spacing(3)
+	},
+	rootBig: {
+		padding: theme.spacing(6)
+	},
+	title: {
+		fontWeight: 'lighter',
+		textAlign: 'left'
+	},
+	wrapper: {
+		position: 'relative'
+	}
+}))
